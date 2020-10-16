4e69d64afc5ad7685b0a9a6f6fd5c6de8fb37855
kakastg <kakastg9@gmail.com>
2020-10-16 21:23:38 +0200

    Fixed an issue

--- Tutorial/src/com/kakastg/tutorial/events/TutorialEvents.java
+++ Tutorial/src/com/kakastg/tutorial/events/TutorialEvents.java
@@ -1,29 +1,12 @@
 package com.kakastg.tutorial.events;
 
 import com.kakastg.tutorial.inventories.SelectionScreen;
-import com.kakastg.tutorial.items.ItemManager;
 import org.bukkit.*;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.PistonMoveReaction;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.*;
-import org.bukkit.metadata.MetadataValue;
-import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.PermissionAttachment;
-import org.bukkit.permissions.PermissionAttachmentInfo;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.util.BoundingBox;
-import org.bukkit.util.Vector;
-
-import java.io.Console;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
 
 public class TutorialEvents implements Listener {
 
@@ -42,17 +25,6 @@ public class TutorialEvents implements Listener {
 
         Material block = player.getWorld().getBlockAt(x,y-1,z).getType();
     }
-    @EventHandler
-    public static void onRightClick(PlayerInteractEvent event) {
-        if (event.getItem().getItemMeta().equals(ItemManager.wand.getItemMeta())) {
-                    Player player = event.getPlayer();
-                    Entity rightClicked = 
-                    rightClicked.remove();
-                    player.giveExp(rightClicked.getTicksLived() / 5);
-                    player.sendMessage(ChatColor.BOLD + "You killed " + rightClicked.getName());
-                }
-            }
-
 
     @EventHandler
     public static void onClick(InventoryClickEvent e){
